24670b99a5e609ee6db1b2750fbd7b561db58aa3
Zhi Liu <james.zhi.liu@gmail.com>
2017-05-28 17:13:46 -0400

    Configured for ppt debug

--- Src/main.c
+++ Src/main.c
@@ -46,8 +46,12 @@
 #include "cmsis_os.h"
 
 /* USER CODE BEGIN Includes */
+#define DISABLE_CAN_1
+
 #include "serial.h"
+#ifdef DISABLE_CAN_1
 #include "can.h"
+#endif
 #include "can2.h"
 #include "errors.h"
 
@@ -90,7 +94,6 @@ void Error_Handler(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_CAN1_Init(void);
 static void MX_CAN2_Init(void);
 static void MX_WWDG_Init(void);
 void doProcessCan(void const * argument);
@@ -563,7 +566,6 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_USART2_UART_Init();
-  MX_CAN1_Init();
   MX_CAN2_Init();
   MX_WWDG_Init();
 
@@ -577,12 +579,14 @@ int main(void)
 	Serial2_writeBuf(FWVersionNumber);
 	Serial2_writeBuf(bootmsg2);
 
+    #ifdef DISABLE_CAN_1
 	bxCan_begin(&hcan1, &mainCanRxQHandle, &mainCanTxQHandle);
 	bxCan_setTxCallback(cantxcb);
 	bxCan_setErrCallback(canercb);
 	bxCan_addMaskedFilterStd(0,0,0); //catch all
 //    bxCan_addFilterStd(0x056, 0);
 	bxCan_addMaskedFilterExt(0,0,0);
+#endif
 
 	bxCan2_begin(&hcan2, &can2RxQHandle, &can2TxQHandle);
 	bxCan2_setTxCallback(cantxcb);
@@ -692,12 +696,11 @@ void SystemClock_Config(void)
 
     /**Initializes the CPU, AHB and APB busses clocks 
     */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
   RCC_OscInitStruct.PLL.PLLN = 160;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 2;
@@ -711,7 +714,7 @@ void SystemClock_Config(void)
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLRCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
@@ -761,11 +764,11 @@ static void MX_CAN2_Init(void)
 {
 
   hcan2.Instance = CAN2;
-  hcan2.Init.Prescaler = 5;
+  hcan2.Init.Prescaler = 10;
   hcan2.Init.Mode = CAN_MODE_NORMAL;
-  hcan2.Init.SJW = CAN_SJW_1TQ;
-  hcan2.Init.BS1 = CAN_BS1_13TQ;
-  hcan2.Init.BS2 = CAN_BS2_2TQ;
+  hcan2.Init.SJW = CAN_SJW_3TQ;
+  hcan2.Init.BS1 = CAN_BS1_11TQ;
+  hcan2.Init.BS2 = CAN_BS2_4TQ;
   hcan2.Init.TTCM = DISABLE;
   hcan2.Init.ABOM = DISABLE;
   hcan2.Init.AWUM = DISABLE;
